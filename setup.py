df723a1b802ada17ae10476270b8b0c09759c6dc
Zachary Wilson <wilsonze@gmail.com>
2018-11-15 09:57:09 -0600

    Add auth and blog blueprints

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read().strip()
 
 with open(path.join(here, 'blog', 'VERSION'), encoding='utf-8') as f:
-    __version__ = f.read().strip()
+    version = f.read().strip()
 
 
 setup(
@@ -19,11 +19,28 @@ setup(
     install_requires=[
         'flask',
     ],
+    license='GPLv3',
     long_description=long_description,
     name='blog',
     packages=find_packages(),
+    # XXX: setuptools_scm seems to cause some issues with the docker build
     # use_scm_version=True,
     # setup_requires=['setuptools_scm'],
-    version=__version__,
+    version=version,
     zip_safe=False,
+    url='https://github.com/zazazack/blog',
+    classifiers=[
+        # https://pypi.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Flask',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Other Scripting Engines',
+        'Programming Language :: PL/SQL',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+    ],
 )
